3d322cea0bf0b2faa47825cf8966957712f0ccf7
Elvin <elvin@arokee.com>
2025-01-16 11:03:50 +0530

    [fixes]:mongoDB auth err fix

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FullStack-Chat-app
+
 This is an implementation of socket.io with MERN stact and render for deployment
+
+live server Link = "https://fullstack-chat-app-l5bt.onrender.com"
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
